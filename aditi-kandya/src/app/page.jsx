3654af0b85b82f8f10e9d2131b6e10a88f14bc5b
Aadeesh Jain <158761421+AadeeshJainB2K@users.noreply.github.com>
2025-06-09 12:07:02 +0000

    navbar partially completed

--- aditi-kandya/src/app/page.jsx
+++ aditi-kandya/src/app/page.jsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
-
+"use client";
+import React, { useState, useEffect } from "react";
+import Header from "./Components/Header/Header.jsx";
 export default function Home() {
   return (
-    
+    <>
+      <Header></Header>
+    </>
   );
 }
