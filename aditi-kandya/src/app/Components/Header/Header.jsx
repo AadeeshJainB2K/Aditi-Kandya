e03ebfa4e42e051be2af0bdf2c943ce8024bc051
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-06-09 17:14:35 +0530

    +1

---
+++ aditi-kandya/src/app/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div id="heading">Aditi Kandya</div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
